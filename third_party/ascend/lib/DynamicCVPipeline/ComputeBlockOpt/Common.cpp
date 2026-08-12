0e9a1429ff8bd8ba292368dd706ac22d503c87e7
suzuko <linzirui1@huawei.com>
2026-08-12 14:54:08 +0800

    migrate SinkI1ProducersIntoUsersPass, BroadcastUBOptPass, MoveLoadIntoUserPass from YY/cut and add checks to skip scf ops and multi-result ops in SinkI1

--- third_party/ascend/lib/DynamicCVPipeline/ComputeBlockOpt/Common.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/ComputeBlockOpt/Common.cpp
@@ -24,8 +24,11 @@
 #include "ascend/include/DynamicCVPipeline/Common/Utils.h"
 #include "ascend/include/DynamicCVPipeline/PlanComputeBlock/Common.h"
 #include "mlir/Analysis/TopologicalSortUtils.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/Operation.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/ViewLikeInterface.h"
 #include "triton/Analysis/Utility.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/DenseSet.h"
@@ -181,10 +184,10 @@ bool willCreateCycle(llvm::ArrayRef<Operation *> opsToUnify,
 }
 
 void cloneScalarOpsForCrossBlockUses(ComputeBlockIdManager &bmOriginal,
-                                     SetVector<Operation *> &matchedOps,
+                                     SetVector<Operation *> &matchedOps, 
                                      int targetBlockId) {
 
-  // This means move matchedOps into targetBlockId
+  // This means move matchedOps into targetBlockId 
   auto sorted = mlir::topologicalSort(matchedOps);
   for (Operation *op : llvm::reverse(sorted)) {
     if (op->getNumResults() == 1 &&
@@ -198,13 +201,12 @@ void cloneScalarOpsForCrossBlockUses(ComputeBlockIdManager &bmOriginal,
             CVPipeline::getAncestorInBlock(userOp, op->getBlock());
         if (!userInBlock)
           continue;
-        if (llvm::find(matchedOps, userInBlock) == matchedOps.end() &&
-            bmOriginal.getBlockIdByOp(userInBlock) != targetBlockId) {
+        if (llvm::find(matchedOps, userInBlock) == matchedOps.end() && bmOriginal.getBlockIdByOp(userInBlock) != targetBlockId) {
           otherUses.push_back(&use);
         }
       }
       if (otherUses.size() > 0) {
-        LOG_DEBUG("now cloned: " << *op);
+        LOG_DEBUG("now cloned: " << *op );
         OpBuilder builder(op);
         auto clonedOp = builder.clone(*op);
         bmOriginal.updateBlockId(clonedOp, bmOriginal.getBlockIdByOp(op));
@@ -214,6 +216,41 @@ void cloneScalarOpsForCrossBlockUses(ComputeBlockIdManager &bmOriginal,
       }
     }
   }
+}
+
+bool isSubviewFromGlobalMemory(ViewLikeOpInterface viewOp,
+                               SetVector<Operation *> &matchedOps) {
+  // Subview ops may be nested many layers deep through reinterpretation or
+  // other subviews. like, subview (subview (reinterpret_cast (subview
+  // (reinterpret_cast (arg0))))) so we need Search and only keep same block
+  // view-like op.
+  Value source = viewOp.getViewSource();
+  auto block = viewOp->getBlock();
+  LOG_DEBUG("Check view source: " << source << "\n");
+  while (true) {
+    if (auto blockArg = dyn_cast<BlockArgument>(source)) {
+      Operation *parentOp = blockArg.getOwner()->getParentOp();
+      if (isa<func::FuncOp>(parentOp)) {
+        return true;
+      } else {
+        LOG_DEBUG(
+            "Subview source block argument is not from func entry block.");
+        return false;
+      }
+    }
+    // From other view-like op
+    if (auto viewLike = dyn_cast<ViewLikeOpInterface>(source.getDefiningOp())) {
+      if (viewLike->getBlock() == block) {
+        matchedOps.insert(viewLike.getOperation());
+      }
+      source = viewLike.getViewSource();
+      continue;
+    }
+    LOG_DEBUG(
+        "Subview source defining op is not ViewLikeOpInterface: " << source);
+    return false;
+  }
+  return false;
 }
 
 } // namespace CVPipeline
