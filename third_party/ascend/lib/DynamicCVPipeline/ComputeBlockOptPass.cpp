7a67aa11fe7f30a5a1bae9c4a20902bbb3211e52
shaoyiyang <shaoyiyang1@huawei.com>
2026-09-02 12:14:19 +0000

    Revert "[ssbuffer](feat) add merge cube block pass (#1849)"
    This reverts commit c0d81e262332992c12e1bf2f3dd8974be22b51e8.

--- third_party/ascend/lib/DynamicCVPipeline/ComputeBlockOptPass.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/ComputeBlockOptPass.cpp
@@ -25,7 +25,6 @@
 #include "DynamicCVPipeline/PlanComputeBlock/Passes.h"
 #include "DynamicCVPipeline/PlanComputeBlock/ReorderOpsByBlockId.h"
 #include "ascend/include/DynamicCVPipeline/Common/Utils.h"
-#include "ascend/include/DynamicCVPipeline/ComputeBlockOpt/MergeCubeBlockPass.h"
 #include "ascend/include/DynamicCVPipeline/ComputeBlockOpt/Passes.h"
 #include "ascend/include/DynamicCVPipeline/PlanComputeBlockPass.h"
 
@@ -78,7 +77,6 @@ void ComputeBlockOptPass::runOnOperation() {
   pm.addPass(createReorderOpsByBlockIdPass());
 
   pm.addPass(createMergeComputeBlockPass());
-  pm.addPass(createMergeCubeBlockPass());
   pm.addPass(createReorderOpsByBlockIdPass());
 
   pm.addPass(createRelocateMemrefDeclPass());
@@ -117,7 +115,6 @@ void registerComputeBlockOptPasses() {
   registerPass(createMergeSmallBlockPass);
   registerPass(createSplitIfByBlockIdPass);
   registerPass(createMergeComputeBlockPass);
-  registerPass(createMergeCubeBlockPass);
   registerPass(createRelocateMemrefDeclPass);
 }
 
