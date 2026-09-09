4858d5c2a72a6bce7c7450963214be8c0e7c03cf
lrf22 <liurf1647@163.com>
2026-09-09 19:20:34 +0800

    [ssbuffer](fix) move fixpipe into splittedif

--- third_party/ascend/include/DynamicCVPipeline/SplitDataflow/InterCoreTransferAndSync.h
+++ third_party/ascend/include/DynamicCVPipeline/SplitDataflow/InterCoreTransferAndSync.h
@@ -169,6 +169,8 @@ private:
                                            int iniProducerBlockId);
   mlir::Operation *getFixpipePointAfterProducer(Value depValue,
                                                 int iniProducerBlockId);
+  mlir::Operation *findYieldMatmulInSplitIf(Value splittedIfResult);
+  void AnalyzeSplittedIf(DependencyInfo &dep);
   mlir::Operation *insertVectorToCubeTransfer(
       mlir::OpBuilder &builder, mlir::Value srcValue,
       mlir::Value normalizedValue, mlir::Operation *vectorEndOp,
